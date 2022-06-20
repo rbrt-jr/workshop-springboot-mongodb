af27c13a2030902db377a9ae735a07e3d1a1d7a9
Roberto Silva <roberto_silva_jr@outlook.pt>
2022-06-20 21:28:32 +0100

    Project created

---
+++ src/test/java/com/rbrt/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbrt.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
