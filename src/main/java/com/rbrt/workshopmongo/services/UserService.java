81af55f856bff1c929753d6a75b0ba574d500447
Roberto Silva <roberto_silva_jr@outlook.pt>
2022-06-21 20:56:08 +0100

    Inserção de usuário com POST

--- src/main/java/com/rbrt/workshopmongo/services/UserService.java
+++ src/main/java/com/rbrt/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rbrt.workshopmongo.domain.User;
+import com.rbrt.workshopmongo.dto.UserDTO;
 import com.rbrt.workshopmongo.repository.UserRepository;
 import com.rbrt.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
