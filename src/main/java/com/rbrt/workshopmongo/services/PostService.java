8354b8d210c5a60fee2778d0b76525b146c985d7
Roberto Silva <roberto_silva_jr@outlook.pt>
2022-06-22 22:16:15 +0100

    Consulta simples com @Query

--- src/main/java/com/rbrt/workshopmongo/services/PostService.java
+++ src/main/java/com/rbrt/workshopmongo/services/PostService.java
@@ -21,8 +21,10 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
 	}
 	
-	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
 	}
+	
+	
 
 }
