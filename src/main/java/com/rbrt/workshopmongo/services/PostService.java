8d1c333fdc7eb07635e29ce2389caf49b72cf453
Roberto Silva <roberto_silva_jr@outlook.pt>
2022-06-22 21:14:07 +0100

    Obtendo um post por id

---
+++ src/main/java/com/rbrt/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.rbrt.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rbrt.workshopmongo.domain.Post;
+import com.rbrt.workshopmongo.repository.PostRepository;
+import com.rbrt.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+	}
+
+}
